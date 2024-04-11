d720a4cc9fd4516e8d3e2f2545a8b586bfa60e5f
LPGLed <166608948+LPGLed@users.noreply.github.com>
2024-04-11 17:12:37 +0200

    Update test_ota.py

--- test_ota.py
+++ test_ota.py
@@ -1,6 +1,8 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
+print("Ciao Marco")
+
 firmware_url = "https://github.com/LPGLed/prova"
 
 ota_updater = OTAUpdater(LPGLed WiFi, !lpgled!, firmware_url, "test_ota.py")
