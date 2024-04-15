79306611841d11ee53d7865342440a5176c1205b
LPGLed <166608948+LPGLed@users.noreply.github.com>
2024-04-15 11:03:18 +0200

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,10 @@
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+print("Ciao Marco")
+
+firmware_url = "https://github.com/LPGLed/prova"
+
+ota_updater = OTAUpdater(LPGLed WiFi, !lpgled!, firmware_url, "test_ota.py")
+
+ota_updater.download_and_install_update_if_available()
