bcb68ac28f4a333f9a6ea1961b68116640fed9db
marco <166608948+LPGLed@users.noreply.github.com>
2024-04-18 17:05:53 +0200

    Update test_ota.py

--- test_ota.py
+++ test_ota.py
@@ -4,8 +4,3 @@ from time import sleep
 #firmware_url = "https://github.com/LPGLed/prova"
 led = Pin(22,Pin.OUT)
 
-while True:
-    print("Ciao Marco!!!!!!!")
-    sleep(1)
-    print("forza Roma!!!!!!!!!!!!!!!!!!")
-    sleep(1)
