7b9c238c5fd0728f5af53c41a9b36576174f3fa6
marco <166608948+LPGLed@users.noreply.github.com>
2024-04-18 14:55:49 +0200

    Update test_ota.py

--- test_ota.py
+++ test_ota.py
@@ -5,6 +5,6 @@ print("Ciao Marco")
 
 firmware_url = "https://github.com/LPGLed/prova"
 
-ota_updater = OTAUpdater(LPGLed WiFi, !lpgled!, firmware_url, "test_ota.py")
+#ota_updater = OTAUpdater(LPGLed WiFi, !lpgled!, firmware_url, "test_ota.py")
 
-ota_updater.download_and_install_update_if_available()
+#ota_updater.download_and_install_update_if_available()
